f40bf7f13d183109efc597f095b17ee945e15fee
Christian Maddox <cmaddox5@vols.utk.edu>
2016-08-23 21:34:53 +0000

    Added About page with working links.

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -18,6 +18,9 @@ class QuotesController < ApplicationController
     end
   end
 
+  def about
+  end
+
   private
 
   def quote_params
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   resources :quotes
 
+  get 'about', to: 'quotes#about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
